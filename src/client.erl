ab9d0d1aa3a06767473956e538d0c828ccdb2325
xiaotie <wudixiaotie@gmail.com>
2015-09-12 10:01:31 +0800

    update

--- src/client.erl
+++ src/client.erl
@@ -29,19 +29,19 @@ init([User]) ->
     {ok, Socket} = gen_tcp:connect ("localhost", 1987, [{packet,0}, {active, true}]),
     % {ok, Socket} = gen_tcp:connect ("192.168.1.137", 1987, [{packet,0}, {active, true}]),
     State = #state{socket = Socket, user = User},
-    Msg = <<"[r] id=\"a_01\" c=\"login\" [r.user] id=\"", (User#user.id)/binary, "\" device=\"", (User#user.device)/binary, "\"">>,
+    Msg = <<"[r] id = \"a_01\" c = \"login\" [r.user] id = \"", (User#user.id)/binary, "\" device = \"", (User#user.device)/binary, "\"">>,
     gen_tcp:send(State#state.socket, Msg),
     io:format ("===client login!~n"),
     {ok, State}.
 
 
 handle_call(send_msg, _From, #state{user = User} = State) ->
-    Msg = <<"[m] id=\"a_02\" c=\"hello\" [m.from] id=\"", (User#user.id)/binary, "\" device=\"", (User#user.device)/binary, "\" [m.to] id=\"2\" device=\"@ipad\"">>,
+    Msg = <<"[m] id = \"a_02\" c = \"hello\" [m.from] id = \"", (User#user.id)/binary, "\" device = \"", (User#user.device)/binary, "\" [m.to] id = \"2\" device = \"@ipad\"">>,
     Result = gen_tcp:send(State#state.socket, Msg),
     io:format ("===client send msg!~n"),
     {reply, Result, State};
 handle_call(send_group_msg, _From, #state{user = User} = State) ->
-    Msg = <<"[gm] id=\"a_03\" c=\"hello\" [gm.user] id=\"", (User#user.id)/binary, "\" device=\"", (User#user.device)/binary, "\" [gm.group] id=\"123\"">>,
+    Msg = <<"[gm] id = \"a_03\" c = \"hello\" [gm.user] id = \"", (User#user.id)/binary, "\" device = \"", (User#user.device)/binary, "\" [gm.group] id = \"123\"">>,
     Result = gen_tcp:send(State#state.socket, Msg),
     io:format ("===client send msg!~n"),
     {reply, Result, State};
